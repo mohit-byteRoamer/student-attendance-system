188409db35a618fc570229fd5ad05a18479f99c6
Mohit Kaushik <94753656+mohit-cod@users.noreply.github.com>
2023-01-07 13:31:36 +0530

    add favicon

--- src/components/shared/studentCard/index.js
+++ src/components/shared/studentCard/index.js
@@ -2,7 +2,11 @@ import "./index.css";
 let StudentCard = function (props) {
   return (
     <div className="studentCardRoot">
-      <div> Roll Number : {props.rollNumber}</div>
+      {props.rollNumber > 0 ? (
+        <div> Roll Number : {props.rollNumber}</div>
+      ) : (
+        <div> No Data</div>
+      )}
     </div>
   );
 };
